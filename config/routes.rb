f69ab8b1b588353ec4090ffb8c4d17bb3b9184a4
José Alves <jose.tapadas@gmail.com>
2021-05-07 13:16:44 +0200

    Added the post view page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :authors
   
   root to: "readers/home#index"
-  get '/blog', to: 'readers/home#blog'
+  
+  get '/blog', to: 'readers/home#blog', as: :blog
+  get '/blog/:id', to: 'readers/posts#show', as: :blog_post
   
   scope module: 'authors' do
     resources :posts do
